7ce07143cd78bb3090797d6e64f9ebe22c9f2a76
dima <tchuryuomv@icloud.com>
2022-08-12 18:03:58 +0300

    new version

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.tasks_list, name='list'),
     path('create/', views.task_create, name='create'),
-    path('add-task/', views.add_task, name="api-add-task"),
+    path('add-task/', views.task_create, name="api-add-task"),
     path('complete/<int:uid>', views.complete_task, name='complete'),
-    path('delete/<int:uid>', views.delete_task, name='delete'),
+    path('api/delete/<int:uid>', views.delete_task, name='delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from .forms import AddTaskForm, TodoItemForm
 
 from tasks.models import TodoItem
 
@@ -19,24 +20,23 @@ def tasks_list(request):
 
 
 def task_create(request):
-    return render(request, "tasks/create.html")
+    if request.method == 'POST':
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/tasks/list")
+    else:
+        form = TodoItemForm()
+
+    return render(request, "tasks/create.html", {"form":form})
 
 
-def complete_task(request, uid):
-    print(uid)
+def complete_task(request, uid): 
     return HttpResponse('Ok')
 
 
 def delete_task(request, uid):
-
     t = TodoItem.objects.get(id=uid)
     t.delete()
     return redirect('/tasks/list')
 
-
-def add_task(request):
-    if request.method == "POST":
-        desc = request.POST["description"]
-        t = TodoItem(description=desc)
-        t.save()
-    return redirect("/tasks/list")

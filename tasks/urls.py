7b50e4c0467cb464d139e3460c2a2e1cb41d49e9
dima <tchuryuomv@icloud.com>
2022-08-13 20:05:49 +0300

    new version

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('list/', views.tasks_list, name='list'),
-    path('create/', views.task_create, name='create'),
-    path('add-task/', views.task_create, name="api-add-task"),
+    path('list/', views.TaskListView.as_view(), name='list'),
+    path('create/', views.TaskCreateView.as_view(), name='create'),
     path('complete/<int:uid>', views.complete_task, name='complete'),
     path('api/delete/<int:uid>', views.delete_task, name='delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,34 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import AddTaskForm, TodoItemForm
-
+from .forms import TodoItemForm
 from tasks.models import TodoItem
+from django.views import View
+from django.views.generic import ListView
 
 
 def index(request):
     return HttpResponse('Примитивный ответ из приложение tasks')
-
-
-def tasks_list(request):
-    all_tasks = TodoItem.objects.all()
-    return render(
-        request,
-        'tasks/list.html',
-        {'tasks': all_tasks}
-    )
-
-
-def task_create(request):
-    if request.method == 'POST':
-        form = TodoItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/tasks/list")
-    else:
-        form = TodoItemForm()
-
-    return render(request, "tasks/create.html", {"form":form})
 
 
 def complete_task(request, uid): 
@@ -40,3 +20,21 @@ def delete_task(request, uid):
     t.delete()
     return redirect('/tasks/list')
 
+class TaskListView(ListView):
+    queryset = TodoItem.objects.all()
+    context_object_name = "tasks"
+    template_name = "tasks/list.html"
+
+
+class TaskCreateView(View):
+    def post(self, request, *args, **kwargs):
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/tasks/list")
+
+        return render(request, "tasks/create.html", {"form": form})
+
+    def get(self, request, *args, **kwargs):
+        form = TodoItemForm()
+        return render(request, "tasks/create.html", {"form": form})
